e89ddee6a7a1bdd54feb8dbbd3ebae8a9c2ae4e5
Felipe Batista <hellojavaa@gmail.com>
2024-03-13 12:02:08 -0300

    re-build: change mode to export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    //change export mode
+    distDir: 'build',
+    output: "export"
 }
 
 module.exports = nextConfig
